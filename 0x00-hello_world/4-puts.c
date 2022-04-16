342a01344c4e1c0938fb96e2892633a7bd4e0b14
francispeps <francispeprah4@gmail.com>
2022-04-16 13:09:40 -0700

    putc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-/**
+/*
  * main - entry point
+ *
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+
+}
