6235c6f349a6d22ac0fc69092049e64f27dfb1eb
francispeps <francispeprah4@gmail.com>
2022-04-18 02:41:06 -0700

    pos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  main - entry point
- *  Description: prints "is it positive or negative"
- *  Return: 0
+ * main - entry point
+ * Description: prints "is it positive or negative"
+ * Return: 0
  */
 int main(void)
 {
