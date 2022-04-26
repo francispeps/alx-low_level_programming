7aa7f29320ccecf73955631591e9aa758c82c82a
francispeps <francispeprah4@gmail.com>
2022-04-26 09:04:04 -0700

    prt

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,33 @@
+#include "main.h"
 
+/**
+ * print_diagonal - Entry point
+ * @n: number of times to print '\' in diagonal
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int i;
+	char k;
+	char s;
+	int j;
+
+	k = '\\';
+	s = ' ';
+	for (i = 0; i < n; i++)
+	{
+		if (n > 1)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(s);
+			}
+			_putchar(k);
+			putchar('\n');
+		}
+	}
+		if (n <= 0)
+		{
+			putchar('\n');
+		}
+}
