47fe576c86e9375bc1c3cdcc418988f588b8b4fc
francispeps <francispeprah4@gmail.com>
2022-05-29 03:50:17 -0700

    rejoice

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,11 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	va_start(valist, n);
+	va_start(valist, n);l
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
-	
+
 	va_end(valist);
 
 	return (sum);
